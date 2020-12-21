dd1129abee3401486d820ace03d5b59b686b6871
tom <stitt4@llnl.gov>
2020-12-21 11:31:36 -0800

    add missing copyright to stream_reader.{ch}pp

--- lib/stream_reader.cpp
+++ lib/stream_reader.cpp
@@ -1,3 +1,14 @@
+// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-443271.
+//
+// This file is part of the GLVis visualization tool and library. For more
+// information and source code availability see https://glvis.org.
+//
+// GLVis is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
 #include "stream_reader.hpp"
 
 using namespace std;
