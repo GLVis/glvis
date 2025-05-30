2a3f8b31b72120ee7a2c145f34e918e6ac3e868c
Jan Nikl <nikl1@llnl.gov>
2025-05-29 17:26:13 -0700

    Enabled compilation threads.cpp.

--- lib/aux_vis.cpp
+++ lib/aux_vis.cpp
@@ -523,7 +523,6 @@ void InitIdleFuncs()
    }
 }
 
-#ifndef __EMSCRIPTEN__
 bool CommunicationIdleFunc()
 {
    int status = glvis_command->Execute();
@@ -539,12 +538,11 @@ bool CommunicationIdleFunc()
    }
    return false;
 }
-#endif
 
 bool MainIdleFunc()
 {
    bool sleep = true;
-#ifndef __EMSCRIPTEN__
+
    if (glvis_command && visualize == 1
        && !(IdleFuncs.Size() > 0 && use_idle))
    {
@@ -567,18 +565,7 @@ bool MainIdleFunc()
       sleep = false;
    }
    use_idle = !use_idle;
-#else
-   if (IdleFuncs.Size() > 0)
-   {
-      LastIdleFunc = (LastIdleFunc + 1) % IdleFuncs.Size();
-      if (IdleFuncs[LastIdleFunc])
-      {
-         (*IdleFuncs[LastIdleFunc])();
-      }
-      // Continue executing idle functions
-      sleep = false;
-   }
-#endif
+
    return sleep;
    LastIdleFunc = (LastIdleFunc + 1) % IdleFuncs.Size();
    if (IdleFuncs[LastIdleFunc])
@@ -1349,9 +1336,7 @@ void ThreadsPauseFunc(GLenum state)
 {
    if (state & KMOD_CTRL)
    {
-#ifndef __EMSCRIPTEN__
       glvis_command->ToggleAutopause();
-#endif
    }
    else
    {
