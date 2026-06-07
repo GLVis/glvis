66a54593792f8229bcfdb0df269cd8d916c85bac
Jan Nikl <nikl1@llnl.gov>
2026-06-06 18:12:06 -0700

    Save coloring suggestion.
    Co-authored-by: John Camier <camierjs@gmail.com>

--- lib/stream_reader.cpp
+++ lib/stream_reader.cpp
@@ -73,7 +73,7 @@ StreamCommands::StreamCommands()
 {
    // Settings
    (*this)[Command::FixOrientations]      = {"fix_orientations", false, "<0/off/1/on>", "Turn off/on fix of the orientations for inverted elements."};
-   (*this)[Command::SaveColoring]         = {"save_coloring", false, "<0/off/1/on>", "Turn off/on saving of the mesh coloring generated when opening only a mesh."};
+   (*this)[Command::SaveColoring]         = {"save_coloring", false, "<0/off/1/on>", "Turn on/off saving of the mesh coloring generated when loading only a mesh."};
    (*this)[Command::KeepAttributes]       = {"keep_attributes", false, "<proc/0/off/real/1/on>", "When loading a parallel solution, toggle between processor ranks and the original (real) attributes."};
    // Solution
    (*this)[Command::Mesh]                 = {"mesh", false, "<mesh>", "Visualize the mesh."};
